90795cc401e64dba1504a55e27ae22e2c6f815e7
HridaySarder <hridoyaug12@gmail.com>
2024-03-31 21:43:00 +0600

    first commit

---
+++ output.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    char c;
+    scanf("%d%c %d%c",&a,&c,&b,&c);
+    printf("%d%% %d%%",a,b);
+}
---
+++ variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    // int rahim = 100;
+    // int karim = 200;
+    // printf("%d %d",rahim,karim);
+
+// float hi = 20.5758;
+// printf("%0.3f",hi);
+
+char ami = 'h';
+printf("%c",ami);
+}
